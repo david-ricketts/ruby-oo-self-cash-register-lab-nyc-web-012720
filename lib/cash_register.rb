dd8fd7acb6e9073a886f4a283153c5efb525b202
David Ricketts <david.ricketts119@gmail.com>
2020-01-31 23:05:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
 
+
+class CashRegister
+  
+  attr_accessor :total 
+  
+  def initialize(employee_discount = 0)
+    @total = 0 
+    
+    
+  end
+  
+
+end
+    
+    
+    
+    
+  # def apply_discount (percentage = 0.80)
+  #   self.total *= percentage
+  # end
+  
+  # def total
+  #   return @total
+  # end
+  
+  
+  # def add_item(title, price, quantity = 1)
+  #   old_tot = @total
+  #   self.total +=  price * quantity
+  #   return "suidndfa #{@total}"
+  # end
+
+  # def apply_discount (discount = 0.80)
+    
+  # end
+  
